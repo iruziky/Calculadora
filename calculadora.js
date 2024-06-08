fe0a44adc35ea4b05b4133ce1f5c95a5945cd3ce
iruziky <112575824+iruziky@users.noreply.github.com>
2024-06-08 07:14:26 -0300

    Create calculadora.js
    Criando um arquivo apenas para o javascript

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+function insert(num) {
+    var numero = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('visor').innerHTML = "";
+}
+
+function calcular() {
+    var resultado = document.getElementById('visor').innerHTML;
+    if (resultado) {
+        document.getElementById('visor').innerHTML = eval(resultado);
+    } else {
+        document.getElementById('visor').innerHTML = "Nada...";
+    }
+}
